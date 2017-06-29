5fc2245c24f29488527cde20cc7f502c09dabbac
Travis Bordovsky <tdb867@tamu.edu>
2017-06-28 21:57:30 -0500

    Add dev and clean server builds to the project directory

---
+++ clean/run.bat
@@ -0,0 +1,2 @@
+java -Xmx1024M -jar craftbukkit-1.11.2.jar -o false
+PAUSE
---
+++ dev/run.bat
@@ -0,0 +1,2 @@
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Xmx256M -jar craftbukkit-1.11.2.jar -o true
+PAUSE
